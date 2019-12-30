0f1eac590cbd0d189d61aeccecb1a61ff1fcb948
EarthenLynx <42932885+EarthenLynx@users.noreply.github.com>
2019-12-30 21:56:24 +0100

    Added logical to show app codes in object. Few minor changes @App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,55 @@
 <template>
 <div id="app">
-  <rgb-input :col="currentColor" @changeRgb="currentColor=$event"></rgb-input>
-  <hex-input :col="currentColor" @changeHex="currentColor=$event"></hex-input>
-  <app-buttons></app-buttons>
-  <app-history :history="colorHistory"></app-history>
-  Color in parent Element: {{ currentColor }}
-  <hr>
-  Computed Rgb Color in parent: {{ colRgb }}
+  <div class="input-container">
+    <hex-input v-if="hexInput" :col="currentColor" @changeHex="currentColor=$event"></hex-input>
+    <rgb-input v-if="!hexInput" :col="currentColor" @changeRgb="currentColor=$event"></rgb-input>
+  </div>
+  <button @click="hexInput = !hexInput"> Toggle between inputs</button>
+  <check-input :col="currentColor" @validColor="addColor()"></check-input>
+  <history :history="colorHistory"></history>
   <br>
-  Computed Hex Color in parent: {{ colHex }}
+  <hr> <br>
+  <h2>Test Zone for parent app</h2>
+  <color-preview :col="currentColor"></color-preview>
+  <p>Current Color in parent Element: {{ currentColor }}</p>
+  <p>Computed Rgb Color in parent: {{ colRgb }}</p>
+  <p>Computed Hex Color in parent: {{ colHex }}</p>
+  <app-buttons></app-buttons>
 </div>
 </template>
 
 <script>
+// Import components
+import ColorPreview from './components/ColorPreview.vue';
+import CheckInput from './components/Input/CheckInput.vue';
 import HexInput from './components/Input/HexInput.vue';
 import RgbInput from './components/Input/RgbInput.vue'
 import AppButtons from './components/AppButtons.vue';
-import AppHistory from './components/AppHistory.vue';
+import History from './components/ColorCollections/History.vue';
+
+// Import color functions
 import {
   hexCheck,
   rgbToHex,
-  hexToRgb
+  hexToRgb,
+  rgbStrToArr
 } from './assets/appColorFunctions.js';
 
 export default {
   components: {
+    ColorPreview,
+    CheckInput,
     AppButtons,
-    AppHistory,
+    History,
     RgbInput,
     HexInput
   },
 
   data: function() {
     return {
+      hexInput: true,
       colorIndex: 0,
-      currentColor: '#2f2f2f', // => This can either be hex OR Rgb
+      currentColor: '', // => This can either be hex OR Rgb
       savedColors: [],
       colorHistory: [],
     }
@@ -47,12 +62,14 @@ export default {
       this.savedColors.push({
         id: this.colorIndex,
         hex: this.colHex,
-        rgb: this.colRgb
+        rgb: this.colRgb,
+        show: true,
       });
-      this.colorHistory.push({
+      this.colorHistory.unshift({
         id: this.colorIndex,
         hex: this.colHex,
-        rgb: this.colRgb
+        rgb: this.colRgb,
+        show: false,
       });
     },
   },
@@ -74,7 +91,9 @@ export default {
     colHex: function() {
       let vm = this;
       if (!hexCheck(vm.currentColor)) {
-        return rgbToHex(vm.currentColor);
+        let tempRgbStr = vm.currentColor;
+        let tempRgbArr = rgbStrToArr(tempRgbStr);
+        return rgbToHex(tempRgbArr[0], tempRgbArr[1], tempRgbArr[2]);
       } else {
         return vm.currentColor;
       }
@@ -91,5 +110,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.input-container {
+  max-height: 60px;
+  padding: 15px;
+}
+
+.collections-container {
+  box-shadow: 1px 1px 1px black;
+  max-height: 120px;
+  padding: 20px 40px;
 }
 </style>
