ba23b10eafd15af5537b2b2cd47ab1b447daab21
EarthenLynx <42932885+EarthenLynx@users.noreply.github.com>
2019-12-30 12:58:11 +0100

    New hextorgb check plus array / string conversion between rgb types

--- src/assets/appColorFunctions.js
+++ src/assets/appColorFunctions.js
@@ -1,22 +1,47 @@
-// Define the checks for the colors
-// By rules of the app: If the color is not hex, it must be rgb
+/*
+* Methods for RGB - Values
+* Includes:
+* 1) Convert rgb values to hex values. Accepts 3 vars
+* 2) Convert rgb String into an array that can be worked with.
+* 3) Convert Rgb Array into a String that's readable by the app.
+*/
+
+// 1) Convert rgb strings to hex string
+export function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// 2) Convert rgb string to rgb arr
+export function rgbStrToArr(rgbStr) {
+  let rgbCols = rgbStr.slice(4, (rgbStr.length - 1));
+  let rgbArr = rgbCols.split(',');
+  return rgbArr;
+}
+
+// 3) Convert rgb arr to rgb string
+export function rgbArrToStr(rgbArr) {
+  let rgbCols = rgbArr.join(',');
+  let rgbStr = 'rgb(' + rgbCols + ')';
+  return rgbStr;
+}
+
+/*
+* Methods for hex - colors
+* 1) Logical check for whether color is hex
+* 2) Convert hex color to rgb color
+*/
+
+// 1) Check if string has hex - format
 export function hexCheck(color) {
   let regExpHex = /^#[0-9a-f]{3,6}$/i;
   return regExpHex.test(color);
 }
 
-// Convert rgb value to hex
-export function rgbToHex(r, g, b) {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-// Convert hex value to rgb
+// 2) Convert hex string to rgb string
+// Adverbiation to: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb#5624139
 export function hexToRgb(hex) {
-  // Change the hex value into an integer
   let hexInt = parseInt(hex.slice(1, hex.length), 16);
-  // Convert each of the hex into decimals, put these into an array
-  let rgbArr = [(hexInt >> 16) & 255, (hexInt >> 8) & 255, hexInt & 255]
-  return rgbArr;
-  // Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb#5624139
-  // Thanks at xameeramir for the explicit answer
+  let rgbArr = [(hexInt >> 16) & 255, (hexInt >> 8) & 255, hexInt & 255].join(',');
+  let rgbStr = 'rgb(' + rgbArr + ')';
+  return rgbStr;
 }
